b091e4f53cc337fa1f0aa09329be92c3d5c6c53a
Joongi Kim <joongi@lablup.com>
2019-06-27 22:06:46 +0900

    Add integration tests which runs while LD_PRELOAD is applied

--- test.sh
+++ test.sh
@@ -4,10 +4,22 @@ set -e
 distro="$1"
 arch="x86_64"
 case $distro in
-  "ubuntu") distro_ver="${distro}16.04" ;;
-  "alpine") distro_ver="${distro}3.8" ;;
+  ubuntu) distro_ver="${distro}16.04" ;;
+  alpine) distro_ver="${distro}3.8" ;;
+  *)
+    echo "Unknown distro value: ${distro}"
+    exit 1
 esac
 
-echo "Running tests for ${distro} ${arch}"
+echo ">> Running unit tests for ${distro} ${arch}"
+docker run --rm -it -v $(pwd):/root \
+           lablup/hook-dev:${distro} \
+           /root/test-hook.${distro_ver}.${arch}.bin
 
-docker run --rm -it -v $(pwd):/root lablup/hook-dev:${distro} /root/test-hook.${distro_ver}.${arch}.bin
+echo ""
+echo ">> Running integration tests for ${distro} ${arch}"
+docker run --rm -it -v $(pwd):/root \
+           --cpuset-cpus=0-1,4-5 \
+           -e LD_PRELOAD=/root/libbaihook.${distro_ver}.${arch}.so \
+           lablup/hook-dev:${distro} \
+           /root/test-hooked.${distro_ver}.${arch}.bin
