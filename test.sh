2cd657568f3b63b8bf7e3155bdfa0dfae1df6a2d
Joongi Kim <joongi@lablup.com>
2019-06-27 14:23:33 +0900

    Fix ubuntu build

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 set -e
-distro=$1
+
+distro="$1"
 arch="x86_64"
+
+# TODO: add version suffix to $distro
+
+echo "Running tests for ${distro} ${arch}"
 
 docker run --rm -it -v $(pwd):/root lablup/hook-dev:${distro} /root/test-hook.${distro}.${arch}.bin
