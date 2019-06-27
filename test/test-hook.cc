b05e896f01922009251fef47615f54bf81980c7c
Joongi Kim <joongi@lablup.com>
2019-06-27 16:05:12 +0900

    Add tests for sysconf hook

--- test/test-hook.cc
+++ test/test-hook.cc
@@ -1,5 +1,10 @@
 #include "utils.hh"
+#include "patch-libs.hh"
+
 #include "gtest/gtest.h"
+
+#include <iostream>
+#include <fstream>
 
 namespace {
 
@@ -7,6 +12,51 @@ TEST(has_prefix, basic) {
     EXPECT_TRUE(has_prefix("a", "abc"));
     EXPECT_TRUE(has_prefix("", ""));
     EXPECT_FALSE(has_prefix("a", "bbc"));
+}
+
+TEST(sysfs_cpuset, normal) {
+    int n;
+    std::string tmp_path = std::tmpnam(nullptr);
+    std::ofstream tmp_file;
+    std::ios_base::openmode mode = std::ofstream::out | std::ofstream::trunc;
+
+    tmp_file.open(tmp_path, mode);
+    tmp_file << "0";
+    tmp_file.close();
+    n = get_num_processors_from_cpuset(tmp_path.c_str());
+    EXPECT_EQ(n, 1);
+
+    tmp_file.open(tmp_path, mode);
+    tmp_file << "0-1";
+    tmp_file.close();
+    n = get_num_processors_from_cpuset(tmp_path.c_str());
+    EXPECT_EQ(n, 2);
+
+    tmp_file.open(tmp_path, mode);
+    tmp_file << "0,1";
+    tmp_file.close();
+    n = get_num_processors_from_cpuset(tmp_path.c_str());
+    EXPECT_EQ(n, 2);
+
+    tmp_file.open(tmp_path, mode);
+    tmp_file << "9,1,2,8";
+    tmp_file.close();
+    n = get_num_processors_from_cpuset(tmp_path.c_str());
+    EXPECT_EQ(n, 4);
+
+    tmp_file.open(tmp_path, mode);
+    tmp_file << "0,1-2";
+    tmp_file.close();
+    n = get_num_processors_from_cpuset(tmp_path.c_str());
+    EXPECT_EQ(n, 3);
+
+    tmp_file.open(tmp_path, mode);
+    tmp_file << "0,2-3,5,7-10";
+    tmp_file.close();
+    n = get_num_processors_from_cpuset(tmp_path.c_str());
+    EXPECT_EQ(n, 8);
+
+    std::remove(tmp_path.c_str());
 }
 
 }
